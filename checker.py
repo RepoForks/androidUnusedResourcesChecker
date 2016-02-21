74b0037779e4883934da4ae86c7525f96bf86adb
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-02-21 11:21:33 +0530

    check : file is present in both hdpi and xhdpi or not

--- checker.py
+++ checker.py
@@ -4,9 +4,12 @@ ROOT_DIRECTORY = '/Users/amitshekhar/Documents/Development/Bobble'
 
 # all file name list
 file_names = set()
+file_names_in_hdpi = set()
+file_names_in_xhdpi = set()
 
 # all files list
 files = set()
+
 
 # add all file names in menu
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/menu'):
@@ -27,12 +30,14 @@ for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBoo
     # add path to all file names.
     for filename in filenames:
         file_names.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
+        file_names_in_hdpi.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
 
 # add all file names in drawable xhdpi
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/drawable-xhdpi'):
     # add path to all file names.
     for filename in filenames:
         file_names.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
+        file_names_in_xhdpi.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
 
 # add all files of layout
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/layout'):
@@ -56,3 +61,14 @@ for file_name in file_names:
                  file_is_present = True
     if not file_is_present:
         print(file_name)
+
+# for files present in hdpi but not in xhdpi
+print('-----------------------------------------------------------------------------')
+print('files present in hdpi but not in xhdpi :')
+print(file_names_in_hdpi.difference(file_names_in_xhdpi))
+
+
+# for files present in xhdpi but not in hdpi
+print('-----------------------------------------------------------------------------')
+print('files present in xhdpi but not in hdpi :')
+print(file_names_in_xhdpi.difference(file_names_in_hdpi))
