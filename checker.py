daa8de9dc05dc2e3e48427b45e43494611aabab4
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-02-21 10:58:22 +0530

    set added for duplicate file

--- checker.py
+++ checker.py
@@ -3,48 +3,48 @@ import os
 ROOT_DIRECTORY = '/Users/amitshekhar/Documents/Development/Bobble'
 
 # all file name list
-file_names = []
+file_names = set()
 
 # all files list
-files = []
+files = set()
 
 # add all file names in menu
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/menu'):
     # add path to all file names.
     for filename in filenames:
-        files.append(os.path.join(dirname, filename)) # add all files of menu
-        file_names.append(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
+        files.add(os.path.join(dirname, filename)) # add all files of menu
+        file_names.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
 
 # add all file names in drawable
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/drawable'):
     # add path to all file names.
     for filename in filenames:
-        files.append(os.path.join(dirname, filename)) # add all files of drawable
-        file_names.append(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
+        files.add(os.path.join(dirname, filename)) # add all files of drawable
+        file_names.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
 
 # add all file names in drawable hdpi
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/drawable-hdpi'):
     # add path to all file names.
     for filename in filenames:
-        file_names.append(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
+        file_names.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
 
 # add all file names in drawable xhdpi
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/drawable-xhdpi'):
     # add path to all file names.
     for filename in filenames:
-        file_names.append(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
+        file_names.add(os.path.splitext(os.path.basename(os.path.join(dirname, filename)))[0])
 
 # add all files of layout
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/layout'):
     for filename in filenames:
-        files.append(os.path.join(dirname, filename))
+        files.add(os.path.join(dirname, filename))
 
 # add all files of java
 for dirname, dirnames, filenames in os.walk(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/java/com/touchtalent/bobbleapp'):
     for filename in filenames:
-        files.append(os.path.join(dirname, filename))
+        files.add(os.path.join(dirname, filename))
 
-files.append(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/raw/seed.json')
+files.add(ROOT_DIRECTORY+'/comic-book/ComicBook/app/src/main/res/raw/seed.json')
 
 # take a file name one by one and search in drawable, layout and java one by one
 for file_name in file_names:
